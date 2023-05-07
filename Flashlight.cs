7cc57a1b4dc62c5d83a81c339478ffab5182496e
Red Magnos <redmagnos@gmail.com>
2023-05-07 15:06:54 -0300

    Switch flashlights from Udon Graph to UdonShart

---
+++ Flashlight.cs
@@ -0,0 +1,41 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Flashlight : UdonSharpBehaviour
+{
+    public new GameObject light;
+    [UdonSynced] public bool isActive = true;
+    private bool isActiveLocal = true;
+    public AudioSource clickSound;
+
+    void Start() {}
+
+    public override void OnPickupUseDown ()
+    {
+        this.ToggleNetwork ();
+    }
+
+    private void ToggleNetwork ()
+    {
+        this.isActive = !this.isActive;
+        this.isActiveLocal = this.isActive;
+        this.ToggleLight ();
+    }
+
+    public override void OnDeserialization ()
+    {
+        if (this.isActiveLocal != this.isActive) {
+            this.isActiveLocal = this.isActive;
+            this.ToggleLight ();
+        }
+    }
+
+    private void ToggleLight (bool playSound = true)
+    {
+        this.light.SetActive (this.isActive);
+        if (playSound) this.clickSound.Play ();
+    }
+}
